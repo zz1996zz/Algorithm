4c9742482f735b107989baeff198c946cbcdd126
JeongSu <zz1996zz@naver.com>
2022-11-07 11:01:11 +0900

    solve: 코딩테스트 문제풀이

--- etc/격자판최대합.py
+++ etc/격자판최대합.py
@@ -3,26 +3,16 @@ import sys
 sys.stdin = open("input.txt", "rt")
 
 N = int(input())
-plate = [list(map(int, input().split())) for _ in range(N)]
-max_value = -2147000000
-
-# 행과 열의 합 구하기
+board = [[i for i in list(map(int, input().split()))] for _ in range(N)]
+answer, a, b = -2147000000, 0, 0
 for i in range(N):
-    sum1 = sum2 = 0
+    tmp = 0
     for j in range(N):
-        sum1 += plate[i][j]
-        sum2 += plate[j][i]
-    if sum1 > max_value:
-        max_value = sum1
-    if sum2 > max_value:
-        max_value = sum2
-# 대각과 역대각 합 구하기
-sum1=sum2=0
+        tmp += board[j][i]
+    answer = max(answer, sum(board[i]), tmp)
 for i in range(N):
-    sum1 += plate[i][i]
-    sum2 += plate[i][-i-1]
-if sum1 > max_value:
-    max_value = sum1
-if sum2 > max_value:
-    max_value = sum2
-print(max_value)
+    a += board[i][i]
+    b += board[i][N-1-i]
+answer = max(answer, a, b)
+
+print(answer)
--- etc/수들의합.py
+++ etc/수들의합.py
@@ -2,24 +2,22 @@ import sys
 
 sys.stdin = open("input.txt", "rt")
 
+answer = 0
 N, M = map(int, input().split())
 nlist = list(map(int, input().split()))
-cnt = 0
-nSum = 0
-start, end = 0, 1
-
-while start < N:
-    nSum = sum(nlist[start:end])
-    if nSum == M:
-        cnt += 1
-        start += 1
-        if start == end:
-            end += 1
-    elif nSum < M and end < N+2:
-        end += 1
+i, j = 0, 1
+while i < N:
+    k = sum(nlist[i:j])
+    if M == k:
+        answer += 1
+        i += 1
+        if i == j:
+            j += 1
+    elif k < M and j < N+2:
+        j += 1
     else:
-        start += 1
-        if start == end:
-            end += 1
+        i += 1
+        if i == j:
+            j += 1
 
-print(cnt)
+print(answer)
