53fc5ffcbb92ff68fe17393462b20b0fb5b7b35d
JeongSu <zz1996zz@naver.com>
2022-11-09 13:27:31 +0900

    solve: 코딩테스트 문제 풀이

--- etc/랜선자르기.py
+++ etc/랜선자르기.py
@@ -2,29 +2,18 @@ import sys
 
 sys.stdin = open("input.txt", "rt")
 
-
-def binary_search(lt, rt):
-    global K, N, answer, lines
-    cnt = 0
-    medium = (lt + rt) // 2
-
-    if lt <= rt:
-        for line in lines:
-            if line >= medium:
-                cnt += line // medium
-        if cnt >= N:
-            answer = medium
-            return binary_search(medium + 1, rt)
-        else:
-            return binary_search(lt, medium - 1)
-    else:
-        return
-
-
 K, N = map(int, input().split())
-lines = [int(input()) for _ in range(K)]
-lines.sort()
-answer = 0
-lt, rt = 1, lines[-1]
-binary_search(lt, rt)
+nlist = [int(input()) for _ in range(K)]
+lt, rt = 1, max(nlist)
+
+while lt <= rt:
+    cnt = 0
+    mid = (lt + rt) // 2
+    for i in nlist:
+        cnt += i // mid
+    if cnt >= N:
+        answer = mid
+        lt = mid + 1
+    else:
+        rt = mid -1
 print(answer)
--- etc/이분검색.py
+++ etc/이분검색.py
@@ -2,25 +2,17 @@ import sys
 
 sys.stdin = open("input.txt", "rt")
 
-
-def binary_search(search_list, find_data):
-    if search_list[0] == find_data:
-        return 0
-    elif search_list[len(search_list)-1] == find_data:
-        return len(search_list) - 1
-
-    medium = len(search_list) // 2
-
-    if search_list[medium] == find_data:
-        return medium
-    elif search_list[medium] > find_data:
-        return binary_search(search_list[:medium], find_data)
-    elif search_list[medium] < find_data:
-        return binary_search(search_list[medium:], find_data) + medium
-
-
 N, M = map(int, input().split())
-search_list = list(map(int, input().split()))
-search_list.sort()
-answer = binary_search(search_list, M)
-print(answer+1)
+nlist = list(map(int, input().split()))
+nlist.sort()
+lt, rt = 0, len(nlist) - 1
+
+while lt <= rt:
+    mid = (lt + rt) // 2
+    if nlist[mid] == M:
+        print(mid + 1)
+        break
+    elif nlist[mid] > M:
+        rt = mid - 1
+    else:
+        lt = mid + 1
